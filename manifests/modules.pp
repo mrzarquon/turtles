3e6d7810d80e3583475987bd4bc91bccdd28898a
Chris Barker <cbarker@puppetlabs.com>
2013-05-16 11:01:52 -0700

    uses the right directories

--- manifests/modules.pp
+++ manifests/modules.pp
@@ -1,20 +1,20 @@
 class turtles::modules {
   # uses vcsrepo to ensure the right modules are installed from vcs
 
-  vcsrepo { '/etc/puppetlabs/puppet/environments/production/profile/':
+  vcsrepo { '/etc/puppetlabs/puppet/environments/production/modules/profile/':
   	ensure   => present,
   	provider => git,
   	source   => 'git://github.com/puppetlabs-seteam/puppet-module-profile.git',
     revision => '8d9c28a4096105604a28009362965ab221c8a7d6',
   }
 
-  vcsrepo { '/etc/puppetlabs/puppet/environments/production/turtles/':
+  vcsrepo { '/etc/puppetlabs/puppet/environments/production/modules/turtles/':
   	ensure => present,
   	provider => git,
   	source => 'git://github.com/mrzarquon/turtles.git',
   }
 
-  vcsrepo { '/etc/puppetlabs/puppet/environments/production/tomcat/':
+  vcsrepo { '/etc/puppetlabs/puppet/environments/production/modules/tomcat/':
   	ensure => present,
   	provider => git,
   	source => 'git://github.com/puppetlabs-seteam/puppet-module-tomcat.git',
