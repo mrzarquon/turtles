17c95c8a5e41ade13b617e6f2cee3aab476f9aa6
Chris Barker <cbarker@puppetlabs.com>
2013-05-16 10:04:31 -0700

    removes pick right now

--- manifests/users.pp
+++ manifests/users.pp
@@ -3,12 +3,16 @@ class turtles::users (
   $list_of_users = $turtles::list_of_users
 ) inherits turtles::params {
 
-  create_resources('pe_accounts::user', $list_of_users)
+  #create_resources('pe_accounts::user', $list_of_users)
 
   resources { 'user':
     purge => true,
   }
-  
+
+  user { 'nfsnobody':
+    ensure => present,
+  }
+
   user { 'root':
     ensure   => present,
     password => '$1$f6lfMB7w$JkbGbJQ/mRmbN9FMSonYl1',
