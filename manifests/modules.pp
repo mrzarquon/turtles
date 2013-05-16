9fffd42a1b1769d28e57424aea8a515086965e00
Chris Barker <cbarker@puppetlabs.com>
2013-05-16 10:25:05 -0700

    sets revision

--- manifests/modules.pp
+++ manifests/modules.pp
@@ -2,9 +2,10 @@ class turtles::modules {
   # uses vcsrepo to ensure the right modules are installed from vcs
 
   vcsrepo { '/etc/puppetlabs/puppet/environments/production/profile/':
-  	ensure => latest,
+  	ensure   => present,
   	provider => git,
-  	source => 'git://github.com/puppetlabs-seteam/puppet-module-profile.git'
+  	source   => 'git://github.com/puppetlabs-seteam/puppet-module-profile.git'
+    revision => '89e66c1b11284603457ea31088ace2169532d12b',
   }
 
   vcsrepo { '/etc/puppetlabs/puppet/environments/production/turtles/':
